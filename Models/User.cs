241de85c1ebcd3fa2d7d5dc6f919aeed46120f26
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-05-26 12:41:21 +0100

    feature: add Base Entity, User and UserType

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Interview_Calendar.Models
+{
+	public class User : BaseEntity
+	{
+        [BsonRequired]
+        public string Name { get; set; } = default!;
+        [BsonRequired]
+        public string Email { get; set; } = default!;
+        [BsonRequired]
+        public string Password { get; set; } = default!;
+        [BsonRequired]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UserTypeEnum UserType { get; set; }
+
+    }
+}
+
