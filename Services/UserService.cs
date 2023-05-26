95d5fb7bbcd7157c39a63146f5e9d3b2323671c9
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-05-26 17:01:14 +0100

    feat: add method on service to add user

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using Interview_Calendar.Data;
 using Interview_Calendar.DTOs;
+using Interview_Calendar.Helpers;
 using Interview_Calendar.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
@@ -12,17 +13,19 @@ namespace Interview_Calendar.Services
 	{
         private readonly IMongoCollection<User> _userCollection;
         private readonly IMapper _mapper;
+        private readonly PasswordHasher _passwordHasher;
 
-        public UserService(IOptions<UserDBConfiguration> userConfiguration, IMapper mapper)
+        public UserService(IOptions<UserDBConfiguration> userConfiguration, IMapper mapper, PasswordHasher hasher)
 		{
             _mapper = mapper;
+            _passwordHasher = hasher;
             var mongoClient = new MongoClient(userConfiguration.Value.ConnectionString);
             var userDatabase = mongoClient.GetDatabase(userConfiguration.Value.DatabaseName);
             _userCollection = userDatabase.GetCollection<User>(userConfiguration.Value.UserCollectionName);
 
         }
 
-        public async Task<UserDto?> CreateUserAsync(UserDto userDto)
+        public async Task<UserDto?> CreateUserAsync(UserCreateDto userDto)
         {
             var checkIfUserExists = _userCollection.Find(x => x.Email == userDto.Email);
 
@@ -31,7 +34,20 @@ namespace Interview_Calendar.Services
                 return null;
             }
 
-            //=> await _userCollection.InsertOneAsync(_mapper.Map<User>(userDto))
+            //Encript passwprd
+            userDto.Password = _passwordHasher.Hash(userDto.Password);
+
+            try
+            {
+                await _userCollection.InsertOneAsync(_mapper.Map<User>(userDto));
+
+                return _mapper.Map<UserDto>(userDto);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            
         }
 
         public async Task<UserDto?> GetUserByEmailAsync(string email) => _mapper.Map<UserDto>(await _userCollection.FindAsync(x => x.Email == email));
